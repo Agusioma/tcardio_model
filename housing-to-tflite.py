a1069f4dd1ed0d5b6585c432c00d8a1eaa574bdb
Agusioma <terryterrence200@gmail.com>
2022-05-15 23:49:44 +0300

    temporary tests

--- housing-to-tflite.py
+++ housing-to-tflite.py
@@ -1,3 +1,5 @@
+import os
+
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -30,24 +32,22 @@ class Model(tf.Module):
         self.model = keras.models.Model(inputs=[input], outputs=[output])
 
         self.model.compile(loss="mse", optimizer="sgd")
-        '''self.model.fit(X_train, y_train, epochs=100,
-                       validation_data=(X_valid, y_valid))'''
+        self.model.fit(X_train, y_train, epochs=20,
+                       validation_data=(X_valid, y_valid))
 
     @tf.function(input_signature=[
-        tf.TensorSpec(shape=(X_train.shape[1:]), dtype=tf.float32),
-        tf.TensorSpec(shape=(y_train.shape[1:]), dtype=tf.float32),
+        tf.TensorSpec([None, 8], tf.float32),
+        tf.TensorSpec([None, ], tf.float32),
     ])
     def train(self, x, y):
-        loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
-        with tf.GradientTape() as tape:
+        '''with tf.GradientTape() as tape:
             prediction = self.model(x)
-            #lossy = self.model. (y, prediction)
-            lossy = loss_fn(y, prediction)
+            lossy = self.model.loss(y, prediction)
         gradients = tape.gradient(lossy, self.model.trainable_variables)
         self.model.optimizer.apply_gradients(
             zip(gradients, self.model.trainable_variables))
         result = {"loss": lossy}
-        return result
+        return result'''
 
     @tf.function(input_signature=[
         tf.TensorSpec([None, 8], tf.float32),
@@ -92,9 +92,34 @@ test_labels = tf.keras.utils.to_categorical(y_test)
 train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
 train_ds = train_ds.batch(BATCH_SIZE)
 
-for x, y in train_ds:
-    result = m.train(x, y)
-
+'''for x, y in train_ds:
+    result = m.train(x, y)'''
+m
 #%%
+m.save('/tmp/model.ckpt')
 
+SAVED_MODEL_DIR = "saved_model"
 #%%
+tf.saved_model.save(
+    m,
+    SAVED_MODEL_DIR,
+    signatures={
+        'infer':
+            m.infer.get_concrete_function(),
+        'save':
+            m.save.get_concrete_function(),
+        'restore':
+            m.restore.get_concrete_function(),
+    })
+#%%
+converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_DIR)
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
+    tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
+]
+converter.experimental_enable_resource_variables = True
+tflite_model = converter.convert()
+
+model_file_path = os.path.join('housing_model.tflite')
+with open(model_file_path, 'wb') as model_file:
+    model_file.write(tflite_model)
